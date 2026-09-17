c3206d9759ecf302996664061623bd9eb22953d6
yashipikapa <juliayashika.kong@dbtc-cebu.edu.ph>
2026-09-17 16:01:28 +0800

    feat: Midterms Examination

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -81,7 +81,6 @@ class MyHomePage extends StatelessWidget {
                 ),
               ],
             )
-
           ),
         ),
 
@@ -123,27 +122,357 @@ class MyHomePage extends StatelessWidget {
               size:30,
               color:Colors.redAccent,
               ),
+              SizedBox(width:3),
+              Text(
+                'VALORANT 3',
+                style: TextStyle(
+                fontSize:17,
+                fontFamily: 'sans-serif',
+                fontWeight: FontWeight.bold,
+                color: Colors.white54,
+                ),
+              ),
+            ],
+          ),
+        ),
+       ),
+            
+       Card(
+        color: Colors.black,
+        margin:EdgeInsets.zero,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+        child: Padding(padding: EdgeInsetsGeometry.only
+        (top:0, bottom:15, left:28, right:17,),
+        child: Row(
+          children: [
+            Column(
+              children: [
+                CircleAvatar(
+            backgroundColor: Color.fromARGB(255, 20, 20, 20),
+            radius: 25,
+            child: Icon(Icons.person, color: Colors.redAccent, size:40),                  
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.circle, size: 16, color: Colors.black),
+
+                    SizedBox(width:10),
+                    Icon(Icons.circle, size:18, color: Colors.greenAccent,),
+                  ],
+                )
+              ],
+            ),
+             SizedBox(width:10),
+        Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'MissYouLikeKrazy',
+            style: TextStyle(
+              fontSize: 20,
+              fontFamily: 'sans-serif',
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(height:5),
+
+          Row( 
+            children: [
+              Icon (
+                Icons.monitor, size: 15, color: Colors.white,),
+              SizedBox(width:3),
+                Text(
+                  'Online - VALORANT',
+                style: TextStyle(
+                fontSize:17,
+                fontFamily: 'sans-serif',
+                color: Colors.white54,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),       
+    ]
+  ),
+),
+),
+
+       Card(
+        color: Colors.black,
+        margin:EdgeInsets.zero,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+        child: Padding(padding: EdgeInsetsGeometry.only
+        (top:0, bottom:15, left:28, right:17,),
+        child: Row(
+          children: [
+            Column(
+              children: [
+                CircleAvatar(
+            backgroundColor: Color.fromARGB(255, 20, 20, 20),
+            radius: 25,
+            child: Icon(Icons.person, color: Colors.redAccent, size:40),                  
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.circle, size: 16, color: Colors.black),
+
+                    SizedBox(width:10),
+                     Icon(Icons.play_circle_fill, size: 16, color: Colors.blueAccent),
+                  ],
+                )
+              ],
+            ),
+             SizedBox(width:10),
+        Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'bread',
+            style: TextStyle(
+              fontSize: 20,
+              fontFamily: 'sans-serif',
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(height:5),
+
+          Row( 
+            children: [
+              Icon (
+                Icons.monitor, size: 15, color: Colors.white,),
+              SizedBox(width:3),
+                Text(
+                  'Playing - VALORANT',
+                style: TextStyle(
+                fontSize:17,
+                fontFamily: 'sans-serif',
+                color: Colors.white54,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),       
+    ]
+  ),
+),
+),
+       Card(
+        color: Colors.black,
+        margin:EdgeInsets.zero,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+        child: Padding(padding: EdgeInsetsGeometry.only
+        (top:0, bottom:15, left:28, right:17,),
+        child: Row(
+          children: [
+            Column(
+              children: [
+                CircleAvatar(
+            backgroundColor: Color.fromARGB(255, 20, 20, 20),
+            radius: 25,
+            child: Icon(Icons.person, color: Colors.redAccent, size:40),                  
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.circle, size: 16, color: Colors.black),
+
+                    SizedBox(width:10),
+                     Icon(Icons.play_circle_fill, size: 16, color: Colors.blueAccent),
+                  ],
+                )
+              ],
+            ),
+             SizedBox(width:10),
+        Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'The14th',
+            style: TextStyle(
+              fontSize: 20,
+              fontFamily: 'sans-serif',
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(height:5),
+
+          Row( 
+            children: [
+              Icon (
+                Icons.monitor, size: 15, color: Colors.white,),
+              SizedBox(width:3),
+                Text(
+                  'Playing - VALORANT',
+                style: TextStyle(
+                fontSize:17,
+                fontFamily: 'sans-serif',
+                color: Colors.white54,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),       
+    ]
+  ),
+),
+),
+        Card(
+          color: Colors.black,
+          margin:EdgeInsets.zero,
+          shape: RoundedRectangleBorder(borderRadius:BorderRadius.zero),
+          child: Padding(padding: EdgeInsetsGeometry.only(top: 0, bottom: 16, left: 25, right: 16,),
+          child: Row(
+            children: [
+              SizedBox(width:3),
+              Text(
+                'Online 4',
+                style: TextStyle(
+                fontSize:15,
+                fontFamily: 'sans-serif',
+                color: Colors.white54,
+                ),
+              ),
+            ],
+          ),
+        ),
+       ),
+       Card(
+        color: Colors.black,
+        margin:EdgeInsets.zero,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+        child: Padding(padding: EdgeInsetsGeometry.only
+        (top:0, bottom:15, left:28, right:17,),
+        child: Row(
+          children: [
+            Column(
+              children: [
+                CircleAvatar(
+            backgroundColor: Color.fromARGB(255, 20, 20, 20),
+            radius: 25,
+            child: Icon(Icons.person, color: Colors.white, size:40),                  
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.circle, size: 16, color: Colors.black),
+
+                    SizedBox(width:10),
+                    Icon(Icons.dark_mode, size:18, color: Colors.yellowAccent,),
+                  ],
+                )
+              ],
+            ),
+             SizedBox(width:10),
+        Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Carlvendish',
+            style: TextStyle(
+              fontSize: 20,
+              fontFamily: 'sans-serif',
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(height:5),
+
+          Row( 
+            children: [
+              Icon (
+                Icons.phone_android, size: 15, color: Colors.white,),
+              SizedBox(width:3),
+                Text(
+                  'Away - Riot Mobile',
+                style: TextStyle(
+                fontSize:17,
+                fontFamily: 'sans-serif',
+                color: Colors.white54,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),       
+    ]
+  ),
+),
+),       
+       Card(
+        color: Colors.black,
+        margin:EdgeInsets.zero,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+        child: Padding(padding: EdgeInsetsGeometry.only
+        (top:0, bottom:15, left:28, right:17,),
+        child: Row(
+          children: [
+            Column(
+              children: [
+                CircleAvatar(
+            backgroundColor: Color.fromARGB(255, 20, 20, 20),
+            radius: 25,
+            child: Icon(Icons.person, color: Colors.white, size:40),                  
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.circle, size: 16, color: Colors.black),
+
+                    SizedBox(width:10),
+                    Icon(Icons.dark_mode, size:18, color: Colors.yellowAccent,),
+                  ],
+                )
+              ],
+            ),
+             SizedBox(width:10),
+        Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'D1yah',
+            style: TextStyle(
+              fontSize: 20,
+              fontFamily: 'sans-serif',
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(height:5),
+
+          Row( 
+            children: [
+              Icon (
+                Icons.phone_android, size: 15, color: Colors.white,),
+              SizedBox(width:3),
+                Text(
+                  'Away - Riot Mobile',
+                style: TextStyle(
+                fontSize:17,
+                fontFamily: 'sans-serif',
+                color: Colors.white54,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),       
+    ]
+  ),
+),
+),
             
 
-            ]
-          )
-        ),
         
-
-        ),
-
-
-
-      ],
-    ),
-  )
-
-    
+        
+        
+        ],
+      ),
+      )
     );
   }
 }
-
-
-  
 
  
